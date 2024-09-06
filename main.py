b5211d1d3ad966b17a7fa7d98b4801c3604964e1
blackbrokkoli <kastomat@gmail.com>
2024-09-06 15:59:18 +0300

    allow opening secondary window

--- main.py
+++ main.py
@@ -2,17 +2,25 @@ import webview
 import os
 
 
-def custom_logic(window):
-    window.toggle_fullscreen()
-    window.evaluate_js('alert("Nice one brother")')
-
-
+# Create a class to expose to JavaScript
 class Api():
-  def log(self, value):
-    print(value)
+    def log(self, value):
+        print(value)
+    def openViewEditGoals(self):
+        with open('assets/goal_edit.html', 'r') as file:
+            html = file.read()
+            window = webview.create_window("Edit Goals", html=html, js_api=Api())
+            webview.start(window)
 
-with open ('assets/main.html', 'r') as file:
-    html = file.read()
-    # window = webview.create_window('Woah dude!', html=html)
-    window = webview.create_window("Test", html=html, js_api=Api())
-    webview.start(window)
+
+
+def main():
+    with open ('assets/main.html', 'r') as file:
+        html = file.read()
+        # window = webview.create_window('Woah dude!', html=html)
+        window = webview.create_window("Test", html=html, js_api=Api(), on_top=True, frameless=False, width=300, height=20, transparent=True)
+        webview.start(window)
+
+
+if __name__ == '__main__':
+    main()
