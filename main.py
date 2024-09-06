07e959e77294e7780b11ac8c8e866b74850bb4b7
blackbrokkoli <kastomat@gmail.com>
2024-09-06 15:49:42 +0300

    achieve basic js/python interaction

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 import webview
+import os
 
-window = webview.create_window('Woah dude!', 'assets/main.html')
-webview.start()
+
+def custom_logic(window):
+    window.toggle_fullscreen()
+    window.evaluate_js('alert("Nice one brother")')
+
+
+class Api():
+  def log(self, value):
+    print(value)
+
+with open ('assets/main.html', 'r') as file:
+    html = file.read()
+    # window = webview.create_window('Woah dude!', html=html)
+    window = webview.create_window("Test", html=html, js_api=Api())
+    webview.start(window)
